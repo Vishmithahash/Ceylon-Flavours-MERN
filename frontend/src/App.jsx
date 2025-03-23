32ec24dd2556d957d5a47098875c9403d76f583d
Ushara Chamuditha Kumara <usharakumara20030820@gmail.com>
2025-03-23 17:38:26 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,23 +32,10 @@ function App() {
           <Routes>
 
 
-          <Route path="/pages/home/home" element={<HomePage />} />
-            <Route path="/" element={<AddMenu />} />
-            <Route path="/" element={<Menu />} />
-
-
+            <Route path="/pages/home/home" element={<HomePage />} />
             <Route path="/pages/home/home" element={<HomePage />} />
             <Route path="/" element={<HomePage/>} />
-
-          <Route path="/pages/home/home" element={<HomePage />} />
-            <Route path="/" element={<AddMenu />} />
-
-             
-
-
-            <Route path="/" element={<Menu />} />
-
-
+            <Route path="/pages/home/home" element={<HomePage />} />
             <Route path="/menu" element={<Menu />} />
             <Route path="/addmenu" element={<AddMenu />} />
             <Route path="/menutable" element={<MenuTable />} />
@@ -61,23 +48,16 @@ function App() {
             <Route path="/reviewform" element={<ReviewForm />} />
             <Route path="/reviewspage" element={<ReviewsPage />} />
             <Route path="/adminreviewpage" element={<AdminReviewPage />} />      
-
-
             <Route path="/reviewform" element={<ReviewForm />} />
             <Route path="/reviewspage" element={<ReviewsPage />} />
             <Route path="/adminreviewpage" element={<AdminReviewPage />} />
             <Route path="/delivery" element={<Delivery/>} />
-
             <Route path="/pages/reservation/Reservation" element={<Reservation />} />
             <Route path="/pages/reservation/ReservationBackground" element={<ReservationManagement />} />
             <Route path="/pages/reservation/reservationList" element={<ReservationList />} />
-
-
-
-
-                    <Route path="/pages/reservation/Reservation" element={<Reservation />} />
-                    <Route path="/pages/reservation/ReservationBackground" element={<ReservationManagement />} />
-                    <Route path="/pages/reservation/reservationList" element={<ReservationList />} />
+            <Route path="/pages/reservation/Reservation" element={<Reservation />} />
+            <Route path="/pages/reservation/ReservationBackground" element={<ReservationManagement />} />
+            <Route path="/pages/reservation/reservationList" element={<ReservationList />} />
 
 
           </Routes>
