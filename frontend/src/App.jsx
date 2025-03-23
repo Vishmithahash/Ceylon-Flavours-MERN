b371d59e2056713688f1399769e1ff837ad09b44
Ushara Chamuditha Kumara <usharakumara20030820@gmail.com>
2025-03-23 17:05:18 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,13 +15,10 @@ import ReviewForm from "./pages/review/ReviewForm";
 import ReviewsPage from "./pages/review/ReviewsPage";
 import AdminReviewPage from './pages/review/AdminReviewPage';
 import { CartProvider } from "./context/CartContext"; 
-
 import Reservation from "./pages/reservation/Reservation";
-
 import ReservationList from "./pages/reservation/reservationList";
 import ReservationManagement from "./pages/reservation/ReservationBackground";
 import HomePage from "./pages/home/home";
-
 import "react-datepicker/dist/react-datepicker.css";
 
 
@@ -33,32 +30,23 @@ function App() {
           <Header />
           <Routes>
 
-          <Route path="/pages/home/home" element={<HomePage />} />
-            <Route path="/" element={<AddMenu />} />
-            
-
-            <Route path="/" element={<Menu />} />
+            <Route path="/pages/home/home" element={<HomePage />} />
+            <Route path="/" element={<HomePage/>} />
             <Route path="/menu" element={<Menu />} />
             <Route path="/addmenu" element={<AddMenu />} />
             <Route path="/menutable" element={<MenuTable />} />
             <Route path="/updatemenu/:id" element={<UpdateMenu />} />
-
             <Route path="/cart" element={<AddCart />} />
             <Route path="/place-order" element={<PlaceOrder />} /> 
             <Route path="/admin-orders" element={<AdminOrder />} /> 
             <Route path="/order-status" element={<OrderStatus />} />
             <Route path="/update-order/:id" element={<UpdatePlaceOrder />} />
-
             <Route path="/reviewform" element={<ReviewForm />} />
             <Route path="/reviewspage" element={<ReviewsPage />} />
             <Route path="/adminreviewpage" element={<AdminReviewPage />} />
-
-
-
-            
-                    <Route path="/pages/reservation/Reservation" element={<Reservation />} />
-                    <Route path="/pages/reservation/ReservationBackground" element={<ReservationManagement />} />
-                    <Route path="/pages/reservation/reservationList" element={<ReservationList />} />
+            <Route path="/pages/reservation/Reservation" element={<Reservation />} />
+            <Route path="/pages/reservation/ReservationBackground" element={<ReservationManagement />} />
+            <Route path="/pages/reservation/reservationList" element={<ReservationList />} />
 
 
 
