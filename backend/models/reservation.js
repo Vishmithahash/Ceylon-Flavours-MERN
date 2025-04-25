5cfbe936e9ffad386780c07248a29e647e34e2a5
Danuka <danukbarnett@gmail.com>
2025-04-25 17:42:04 +0530

    Update reservation.js

--- backend/models/reservation.js
+++ backend/models/reservation.js
@@ -2,54 +2,38 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name : {
-        type : String,
-        required : true,
+        type : String, required : true,
     },
 
     email : {
-        type : String,
-        required : true,
+        type : String, required : true,
     },
 
     phone : {
-        type : Number,
-        required : true,
-
+        type : Number, required : true,
 
     },
     date: {
-        type: String,
-        required: true,
+        type: String, required: true,
     },
 
-
     time: {
-        type: String,
-        required: true,
+        type: String, required: true,
     },
 
 
     no_of_guests: {
-        type: Number,
-        required: true,
+        type: Number, required: true,
     },
-    
 
     table_category: {
-        type: String,
-        enum: ["Normal", "VIP"],
-        required: true,
-        default: "Normal",  // Set a default value if none is provided
+        type: String, enum: ["Normal", "VIP"], required: true, default: "Normal",  // Set a default value if none is provided
       },
 
       notes: {
-        type: String,
-        required: false,
+        type: String, required: false,
     },
 
-
 })
-
 const reservation = mongoose.model("Reservations",userSchema)
-
 export default reservation
